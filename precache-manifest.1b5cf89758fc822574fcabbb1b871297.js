3b026e7112f184013c569c966dd7df993e7164cb
Shai Michaeli <shaiws2@gmail.com>
2018-12-06 22:12:39 +0200

    Updates

---
+++ precache-manifest.1b5cf89758fc822574fcabbb1b871297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d504383a6c1ef39ee510",
+    "url": "/tictactoe/static/css/main.5cafd7d7.chunk.css"
+  },
+  {
+    "revision": "d504383a6c1ef39ee510",
+    "url": "/tictactoe/static/js/main.d504383a.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/tictactoe/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "78efc45813b54cd95fc9f1c2d6093b4c",
+    "url": "/tictactoe/index.html"
+  }
+];
